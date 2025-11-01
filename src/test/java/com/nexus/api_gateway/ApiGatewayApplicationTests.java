e69b14ac92b621236c0ef65120906e48e9e5dd20
Mohd Ataa Khan <trendsandfactss@gmail.com>
2025-11-01 19:17:51 +0530

    Updating pom, application.yml and commenting out test class

--- src/test/java/com/nexus/api_gateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/nexus/api_gateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,13 @@
-package com.nexus.api_gateway;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApiGatewayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.nexus.api_gateway;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApiGatewayApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
